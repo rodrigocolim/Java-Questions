4816b13e43dcb0af4bb81e8ea0f48dd0db37ef3c
Rodrigo <rodrigoferlima.12@gmail.com>
2019-12-14 12:04:05 -0300

    get_nth method change
    Change to consider n=0 as first element of series instead of n=1.

--- src/main/q2/Sequence.java
+++ src/main/q2/Sequence.java
@@ -4,12 +4,12 @@ public class Sequence {
 	/**
 	 * Calculates, recursively, the n'th element in a sequence 
 	 * where a n element corresponds to the sum of the previous 
-	 * element with n-1, or 0, in case of the first element
+	 * element with n, or 0, in case of the first element
 	 * @return n'th element
 	 */
 	public static int get_nth(int n){
-		if(n>1) {
-			return get_nth(n-1)+n-1;
+		if(n>0) {
+			return get_nth(n-1)+n;
 		}
 		return 0;
 	}
