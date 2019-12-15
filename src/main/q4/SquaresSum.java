88a28cae0b6ada8a00592b7bd59ed778d57bab2e
Rodrigo Lima <42893977+rodrigocolim@users.noreply.github.com>
2019-12-15 12:22:12 -0300

    Update SquaresSum.java
    Comment correction.

--- src/main/q4/SquaresSum.java
+++ src/main/q4/SquaresSum.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class SquaresSum {
 	
 	/**
-	 *	Checks is a number is prime
+	 *	Checks if a number is prime
 	 *	@number number to check if it's prime
 	 *	@return true if the number is prime or false otherwise
 	 */
@@ -15,10 +15,10 @@ public class SquaresSum {
 			return false;
 		}
 		
-		//define a limit to check
+		//Defines a limit to check
 		int max = (int) Math.sqrt(number);
 		for(int i = 2; i <= max; i++) {
-			//Checks if i divide number
+			//Checks if i divides number
 			if(number%i==0) {
 				return false;
 			}
@@ -28,12 +28,12 @@ public class SquaresSum {
 	}
 	
 	/**
-	 *	Sum the square of all prime numbers in a list
+	 *	Adds the square of all prime numbers in a list
 	 *	@numbers list of numbers to sum 
 	 *	@return the sum of all given prime numbers
 	 */
 	public static int sumSquaresOfPrimes(ArrayList<Integer> numbers) {
-		//Initialize the sum
+		//Initializes the sum
 		int sum = 0;
 		for(Integer number : numbers) {
 			//Checks if number is prime
