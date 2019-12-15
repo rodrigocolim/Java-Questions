e9ea93e8f2384dc2571b43259ba47db69c600b4c
Rodrigo <rodrigoferlima.12@gmail.com>
2019-12-14 22:32:48 -0300

    Inclusion of alternative methods
    Inclusion of two alternative methods for calculating the nth element of a sequence and tests.

--- src/main/q2/Sequence.java
+++ src/main/q2/Sequence.java
@@ -13,4 +13,51 @@ public class Sequence {
 		}
 		return 0;
 	}
+	
+	/**
+	 * Calculates, by tail recursive, the n'th element in a sequence 
+	 * where a n element corresponds to the sum of the previous 
+	 * element with n, or 0, in case of the first element
+	 * @param n position in the sequence
+	 * @param a recursion accumulator
+	 * @return n'th element
+	 */
+	private static int tail_recursive_aux(int n, int a) {
+		if(n==0) {
+			//Returns the accumulator of all recursions as the nth element
+			return a;
+		}
+		//Increments the accumulator and return a new recursion
+		return tail_recursive_aux(n-1, n+a);
+	}
+	
+	/**
+	 * Calculates, by tail recursive, the n'th element in a sequence 
+	 * where a n element corresponds to the sum of the previous 
+	 * element with n, or 0, in case of the first element
+	 * @param n position in the sequence
+	 * @return n'th element
+	 */
+	public static int get_nth_by_tail_recursive(int n) {
+		//Returns the call to a helper function that calculates the nth element
+		//Start recursion accumulator at 0
+		return tail_recursive_aux(n,0);
+	}
+	
+	/**
+	 * Calculates, iteratively, the n'th element in a sequence 
+	 * where a n element corresponds to the sum of the previous 
+	 * element with n, or 0, in case of the first element
+	 * @param n position in the sequence
+	 * @return n'th element
+	 */
+	public static int get_nth_iteratively(int n) {
+		//Initialize the nth
+		int nth = 0;
+		for(int i=0; i<=n; i++) {
+			//Adds the current element with the the iterator to compose the nth element corresponding
+			nth += i;
+		}
+		return nth;
+	}
 }
--- tests/main/q2/SequenceTest.java
+++ tests/main/q2/SequenceTest.java
@@ -2,25 +2,55 @@ package main.q2;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import org.junit.Before;
+
 import static org.hamcrest.CoreMatchers.*;
 
 public class SequenceTest {
 	
+	private int[] sequence;
+	private int sequenceSize;
+	
+	@Before
+	public void setUp() {
+		// Sets a sequence
+		sequenceSize = 7;
+		sequence = new int[sequenceSize];
+		sequence[0]=0;
+		sequence[1]=1;
+		sequence[2]=3;
+		sequence[3]=6;
+		sequence[4]=10;
+		sequence[5]=15;
+		sequence[6]=21;
+	}
+	
 	@Test
 	public void testGetNth() {
-		
-		int[] results = new int[6];
-		results[0]=0;
-		results[1]=1;
-		results[2]=3;
-		results[3]=6;
-		results[4]=10;
-		results[5]=15;
-		
-		for(int s, i = 0; i<6; i++) {
+		// Checks the recursive method for all sequence
+		for(int s, i = 0; i<sequenceSize; i++) {
 			s = Sequence.get_nth(i);
-			assertThat(s,is(results[i]));
+			assertThat(s,is(sequence[i]));
 		}
 		
+	}
+	
+	@Test
+	public void testGetNthByTailRecursive() {
+		// Checks the tail recursive method for all sequence
+		for(int s, i = 0; i<sequenceSize; i++) {
+			s = Sequence.get_nth_by_tail_recursive(i);
+			assertThat(s,is(sequence[i]));
+		}
+	}
+	
+	@Test
+	public void testGetNthIteratively() {
+		// Checks the iterative method for all sequence
+		for(int s, i = 0; i<sequenceSize; i++) {
+			s = Sequence.get_nth_by_tail_recursive(i);
+			assertThat(s,is(sequence[i]));
+		}
 	}
 }
